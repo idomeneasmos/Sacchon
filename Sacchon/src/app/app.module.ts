7313848f1729cd924646db627899e561f25985d8
Eleni Kelaiditi <elenkelaid@gmail.com>
2021-04-11 11:53:31 +0300

    recreate commonents

--- Sacchon/src/app/app.module.ts
+++ Sacchon/src/app/app.module.ts
@@ -3,11 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LogInComponent } from './log-in/log-in.component';
+import { SingUpDoctorComponent } from './sing-up-doctor/sing-up-doctor.component';
+import { SingUpPatientComponent } from './sing-up-patient/sing-up-patient.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LogInComponent,
+    SingUpDoctorComponent,
+    SingUpPatientComponent
   ],
   imports: [
     BrowserModule
