f8ae4f15f48c73beb8429aaa9d0cbebdb3f542e2
Idomeneas Moschakis <idomeneasmos@gmail.com>
2021-04-15 19:16:52 +0300

    temp

--- Sacchon/src/app/log-in/log-in.component.ts
+++ Sacchon/src/app/log-in/log-in.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LogInComponent implements OnInit {
 
-  form:FormGroup;
+  form!:FormGroup;
   constructor(private fb:FormBuilder) {
   }
 
